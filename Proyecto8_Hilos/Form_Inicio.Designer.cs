84bc19c5383912459c95a9ccbdc69853eb7e08e5
nallely-lopez <lopezgarcializethnallely@gmail.com>
2025-04-11 21:04:12 -0600

    Se agregó los componentes perzonalizados de botón, panel y texbox, al igual que se implementó la interfaz de inicio a la aplicación

--- Proyecto8_Hilos/Form_Inicio.Designer.cs
+++ Proyecto8_Hilos/Form_Inicio.Designer.cs
@@ -28,18 +28,229 @@
         /// </summary>
         private void InitializeComponent()
         {
+            customPanel1 = new AppAgenda.Componente_Panel.CustomPanel();
+            customPanel2 = new AppAgenda.Componente_Panel.CustomPanel();
+            customPanel3 = new AppAgenda.Componente_Panel.CustomPanel();
+            customPanel4 = new AppAgenda.Componente_Panel.CustomPanel();
+            customPanel5 = new AppAgenda.Componente_Panel.CustomPanel();
+            label1 = new Label();
+            label2 = new Label();
+            rjButton1 = new CustomControls.RJControls.RJButton();
+            rjButton2 = new CustomControls.RJControls.RJButton();
+            rjButton3 = new CustomControls.RJControls.RJButton();
+            rjButton4 = new CustomControls.RJControls.RJButton();
+            rjButton5 = new CustomControls.RJControls.RJButton();
+            customPanel1.SuspendLayout();
+            customPanel4.SuspendLayout();
+            customPanel5.SuspendLayout();
             SuspendLayout();
+            // 
+            // customPanel1
+            // 
+            customPanel1.BackColor = Color.Teal;
+            customPanel1.BorderColor = Color.Black;
+            customPanel1.BorderRadius = 15;
+            customPanel1.BorderSize = 3;
+            customPanel1.Controls.Add(customPanel5);
+            customPanel1.Controls.Add(customPanel4);
+            customPanel1.Controls.Add(customPanel3);
+            customPanel1.Controls.Add(customPanel2);
+            customPanel1.Location = new Point(-11, -3);
+            customPanel1.Name = "customPanel1";
+            customPanel1.Size = new Size(1499, 1056);
+            customPanel1.TabIndex = 0;
+            // 
+            // customPanel2
+            // 
+            customPanel2.BackColor = Color.DarkSlateGray;
+            customPanel2.BorderColor = Color.Black;
+            customPanel2.BorderRadius = 50;
+            customPanel2.BorderSize = 3;
+            customPanel2.Location = new Point(42, 27);
+            customPanel2.Name = "customPanel2";
+            customPanel2.Size = new Size(1132, 160);
+            customPanel2.TabIndex = 0;
+            // 
+            // customPanel3
+            // 
+            customPanel3.BackColor = Color.LightSeaGreen;
+            customPanel3.BorderColor = Color.Black;
+            customPanel3.BorderRadius = 50;
+            customPanel3.BorderSize = 3;
+            customPanel3.Location = new Point(54, 38);
+            customPanel3.Name = "customPanel3";
+            customPanel3.Size = new Size(1107, 138);
+            customPanel3.TabIndex = 1;
+            // 
+            // customPanel4
+            // 
+            customPanel4.BackColor = Color.PowderBlue;
+            customPanel4.BorderColor = Color.Black;
+            customPanel4.BorderRadius = 15;
+            customPanel4.BorderSize = 3;
+            customPanel4.Controls.Add(label1);
+            customPanel4.Location = new Point(54, 257);
+            customPanel4.Name = "customPanel4";
+            customPanel4.Size = new Size(690, 730);
+            customPanel4.TabIndex = 2;
+            // 
+            // customPanel5
+            // 
+            customPanel5.BackColor = Color.PowderBlue;
+            customPanel5.BorderColor = Color.Black;
+            customPanel5.BorderRadius = 15;
+            customPanel5.BorderSize = 3;
+            customPanel5.Controls.Add(rjButton5);
+            customPanel5.Controls.Add(rjButton4);
+            customPanel5.Controls.Add(rjButton3);
+            customPanel5.Controls.Add(rjButton2);
+            customPanel5.Controls.Add(rjButton1);
+            customPanel5.Controls.Add(label2);
+            customPanel5.Location = new Point(764, 257);
+            customPanel5.Name = "customPanel5";
+            customPanel5.Size = new Size(690, 730);
+            customPanel5.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Stencil", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(35, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 29);
+            label1.TabIndex = 0;
+            label1.Text = "NOTAS";
+            label1.Click += this.label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Stencil", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(32, 36);
+            label2.Name = "label2";
+            label2.Size = new Size(130, 29);
+            label2.TabIndex = 1;
+            label2.Text = "MATERIAS";
+            // 
+            // rjButton1
+            // 
+            rjButton1.BackColor = Color.CadetBlue;
+            rjButton1.BackgroundColor = Color.CadetBlue;
+            rjButton1.BorderColor = Color.Teal;
+            rjButton1.BorderRadius = 40;
+            rjButton1.BorderSize = 3;
+            rjButton1.FlatAppearance.BorderSize = 0;
+            rjButton1.FlatStyle = FlatStyle.Flat;
+            rjButton1.ForeColor = Color.White;
+            rjButton1.Location = new Point(32, 105);
+            rjButton1.Name = "rjButton1";
+            rjButton1.Size = new Size(60, 60);
+            rjButton1.TabIndex = 2;
+            rjButton1.Text = "+";
+            rjButton1.TextColor = Color.White;
+            rjButton1.UseVisualStyleBackColor = false;
+            // 
+            // rjButton2
+            // 
+            rjButton2.BackColor = Color.CadetBlue;
+            rjButton2.BackgroundColor = Color.CadetBlue;
+            rjButton2.BorderColor = Color.Teal;
+            rjButton2.BorderRadius = 40;
+            rjButton2.BorderSize = 3;
+            rjButton2.FlatAppearance.BorderSize = 0;
+            rjButton2.FlatStyle = FlatStyle.Flat;
+            rjButton2.ForeColor = Color.White;
+            rjButton2.Location = new Point(32, 212);
+            rjButton2.Name = "rjButton2";
+            rjButton2.Size = new Size(60, 60);
+            rjButton2.TabIndex = 3;
+            rjButton2.Text = "+";
+            rjButton2.TextColor = Color.White;
+            rjButton2.UseVisualStyleBackColor = false;
+            // 
+            // rjButton3
+            // 
+            rjButton3.BackColor = Color.CadetBlue;
+            rjButton3.BackgroundColor = Color.CadetBlue;
+            rjButton3.BorderColor = Color.Teal;
+            rjButton3.BorderRadius = 40;
+            rjButton3.BorderSize = 3;
+            rjButton3.FlatAppearance.BorderSize = 0;
+            rjButton3.FlatStyle = FlatStyle.Flat;
+            rjButton3.ForeColor = Color.White;
+            rjButton3.Location = new Point(32, 318);
+            rjButton3.Name = "rjButton3";
+            rjButton3.Size = new Size(60, 60);
+            rjButton3.TabIndex = 4;
+            rjButton3.Text = "+";
+            rjButton3.TextColor = Color.White;
+            rjButton3.UseVisualStyleBackColor = false;
+            // 
+            // rjButton4
+            // 
+            rjButton4.BackColor = Color.CadetBlue;
+            rjButton4.BackgroundColor = Color.CadetBlue;
+            rjButton4.BorderColor = Color.Teal;
+            rjButton4.BorderRadius = 40;
+            rjButton4.BorderSize = 3;
+            rjButton4.FlatAppearance.BorderSize = 0;
+            rjButton4.FlatStyle = FlatStyle.Flat;
+            rjButton4.ForeColor = Color.White;
+            rjButton4.Location = new Point(32, 425);
+            rjButton4.Name = "rjButton4";
+            rjButton4.Size = new Size(60, 60);
+            rjButton4.TabIndex = 5;
+            rjButton4.Text = "+";
+            rjButton4.TextColor = Color.White;
+            rjButton4.UseVisualStyleBackColor = false;
+            // 
+            // rjButton5
+            // 
+            rjButton5.BackColor = Color.CadetBlue;
+            rjButton5.BackgroundColor = Color.CadetBlue;
+            rjButton5.BorderColor = Color.Teal;
+            rjButton5.BorderRadius = 40;
+            rjButton5.BorderSize = 3;
+            rjButton5.FlatAppearance.BorderSize = 0;
+            rjButton5.FlatStyle = FlatStyle.Flat;
+            rjButton5.ForeColor = Color.White;
+            rjButton5.Location = new Point(32, 535);
+            rjButton5.Name = "rjButton5";
+            rjButton5.Size = new Size(60, 60);
+            rjButton5.TabIndex = 6;
+            rjButton5.Text = "+";
+            rjButton5.TextColor = Color.White;
+            rjButton5.UseVisualStyleBackColor = false;
             // 
             // Form_Inicio
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1478, 1044);
+            Controls.Add(customPanel1);
             Name = "Form_Inicio";
             Text = "Form1";
+            customPanel1.ResumeLayout(false);
+            customPanel4.ResumeLayout(false);
+            customPanel4.PerformLayout();
+            customPanel5.ResumeLayout(false);
+            customPanel5.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private AppAgenda.Componente_Panel.CustomPanel customPanel1;
+        private AppAgenda.Componente_Panel.CustomPanel customPanel2;
+        private AppAgenda.Componente_Panel.CustomPanel customPanel5;
+        private AppAgenda.Componente_Panel.CustomPanel customPanel4;
+        private Label label1;
+        private AppAgenda.Componente_Panel.CustomPanel customPanel3;
+        private Label label2;
+        private CustomControls.RJControls.RJButton rjButton5;
+        private CustomControls.RJControls.RJButton rjButton4;
+        private CustomControls.RJControls.RJButton rjButton3;
+        private CustomControls.RJControls.RJButton rjButton2;
+        private CustomControls.RJControls.RJButton rjButton1;
     }
 }
