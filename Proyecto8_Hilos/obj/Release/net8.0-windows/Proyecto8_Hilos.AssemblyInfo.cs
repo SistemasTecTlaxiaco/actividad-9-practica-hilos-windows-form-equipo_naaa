9aee2778c72d3d23b6a5c1cf152bdeeb26adb3c0
sahid cortes <spartan092spartan@gmail.com>
2025-04-12 19:02:31 -0600

    Cree la interfaz de crear usuario con sus componentes

--- Proyecto8_Hilos/obj/Release/net8.0-windows/Proyecto8_Hilos.AssemblyInfo.cs
+++ Proyecto8_Hilos/obj/Release/net8.0-windows/Proyecto8_Hilos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto8_Hilos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45fc64410d992ef128b160039875bfe1c830b638")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84bc19c5383912459c95a9ccbdc69853eb7e08e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto8_Hilos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto8_Hilos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
